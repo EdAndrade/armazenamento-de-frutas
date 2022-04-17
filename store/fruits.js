33e7a3305c68214d9c801d7ba569d06fe2d5641d
edAndrade <edmilisonwork@gmail.com>
2022-04-17 23:50:00 +0100

    implemented fruit state management

---
+++ store/fruits.js
@@ -0,0 +1,16 @@
+export const state = () => ({
+    fruit_id_track: 1,
+    fruits: []
+})
+
+export const mutations = {
+
+    CREATE_FRUIT(state, { fruit_name, fruit_price }){
+        state.fruits.push({
+            id: state.fruit_id_track,
+            fruit_name,
+            fruit_price
+        })
+        state.fruit_id_track++
+    }
+}
