26630b3df1e9e386526120d5697e543425618fe8
edAndrade <edmilisonwork@gmail.com>
2022-04-17 23:45:02 +0100

    implemented fruit management in buckets

--- store/buckets.js
+++ store/buckets.js
@@ -5,15 +5,40 @@ export const state = () => ({
 
 export const mutations = {
 
-    CREATE_BUCKET(state,bucket){
+    CREATE_BUCKET(state, { bucket_capacity }){
         state.bucket_id_track++
         state.buckets.push({
             id: state.bucket_id_track,
-            ...bucket
+            bucket_capacity,
+            fruits: []
         })
     },
 
     REMOVE_BUCKET(state, bucket_id){
         state.buckets = state.buckets.filter( bucket => bucket.id !== bucket_id )
+    },
+
+    ADD_FRUIT_TO_BUCKET(state, { bucket_id, fruit_name, fruit_price }){
+        state.buckets.forEach( bucket => {
+            if(bucket.id === bucket_id)
+                bucket.fruits.push({ fruit_name, fruit_price })
+        })
+    },
+
+    REMOVE_FRUIT_FROM_BUCKET(state, { bucket_id, fruit_name_to_remove }){
+
+        let fruitRemoved = false
+
+        for(let i in state.buckets){
+            if(fruitRemoved)
+                break
+            if(bucket_id === state.buckets[i].id){
+                state.buckets[i].fruits = state.buckets[i].fruits.filter( fruit => {
+                    if( fruitRemoved && fruit.fruit_name === fruit_name_to_remove )
+                        return false
+                    return true
+                })
+            }
+        }
     }
 }
