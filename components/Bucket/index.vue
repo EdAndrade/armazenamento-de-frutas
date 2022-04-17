1b473d612ae9bcf3edfc647b02f47a9c66ad1a6b
edAndrade <edmilisonwork@gmail.com>
2022-04-18 00:38:11 +0100

    created bucket component

---
+++ components/Bucket/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="modal-1">
+        <h1>Criar balde</h1>
+        <label for="capacityInput"></label>
+        <input type="text" id="capacityInput">
+        <button>Salvar</button>
+    </section>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+
+</style>
