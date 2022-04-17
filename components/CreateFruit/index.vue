414abaf3b252d47f22261f1d48cfa740022705be
edAndrade <edmilisonwork@gmail.com>
2022-04-18 00:50:52 +0100

    created fruit component

---
+++ components/CreateFruit/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="modal-1">
+        <h1>Criar Fruta</h1>
+        <label for="name">Nome</label>
+        <input type="text" id="name">
+        <label for="price">Preço</label>
+        <input type="text" id="price">
+        <button>Salvar</button>
+    </section>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+
+</style>
