34d44b12c0e15a67ac222b24577b1b2575e2ab51
edAndrade <edmilisonwork@gmail.com>
2022-04-18 01:59:01 +0100

    finished bucket base style

---
+++ components/Bucket/index.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <section class="bucket">
+
+        <div class="gross_bucket">
+            <div class="top">
+                <p class="total"></p>
+                <button class="remove">
+                    
+                </button>
+            </div>
+
+            <div class="fruits">
+                <div class="fruit">
+                    <p class="fruitName">Fruta A</p>
+                    <p class="fruitPrice">R$ 3.90</p>
+                </div>
+            </div>
+        </div>
+
+        <p class="capacity">
+            Capacidade 50%
+        </p>
+    </section>
+
+</template>
+
+<script>
+
+    export default {
+
+    }
+
+</script>
+
+<style scoped lang="scss" src="./style.scss"></style>
