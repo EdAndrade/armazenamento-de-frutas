5f2e33d20eef0e4e09b6cc7717543fd5880690c5
edAndrade <edmilisonwork@gmail.com>
2022-04-18 01:20:54 +0100

    finished fruit style

---
+++ components/Fruit/index.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <section class="fruit">
+
+        <div class="description">
+            <p class="fruitName"></p>
+            <p class="fruitPrice"></p>
+        </div>
+
+        <div class="addToABucket">
+            +
+        </div>
+
+    </section>
+
+</template>
+
+<script>
+
+    export default {
+
+    }
+
+</script>
+
+<style scoped lang="scss" src="./style.scss"></style>
