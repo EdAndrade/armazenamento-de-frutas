6219f91a45521c0734ef6ee85953faab90508e8d
edAndrade <edmilisonwork@gmail.com>
2022-04-19 23:17:58 +0100

    fixed validation

--- components/CreateFruit/index.vue
+++ components/CreateFruit/index.vue
@@ -4,7 +4,7 @@
         <label for="name">Nome</label>
         <input type="text" v-model="fruit.fruit_name" id="name" step="0.01">
         <label for="price">Preço</label>
-        <input type="number" v-model.number="fruit.fruit_price" id="price">
+        <input type="text" v-model="fruit.fruit_price" id="price">
         <button>Salvar</button>
     </form>
 </template>
@@ -27,11 +27,14 @@
         methods: {
 
             createFruit(){
-                if(this.fruit.fruit_name === '' || this.fruit.fruit_price <= 0){
-                    alert('O campo nome deve estar preenchido\nO preço da fruta não pode ser menor que 0')
-                }else{
+
+                this.fruit.fruit_price = parseFloat(this.fruit.fruit_price)
+                if(this.fruit.fruit_name !== '' && ( this.fruit.fruit_price > 0 && typeof(this.fruit.fruit_price) === 'number' )){
                     this.$store.commit('fruits/CREATE_FRUIT', this.fruit)
                     this.cleanFields()
+                }else{
+                    this.fruit.fruit_price = 0
+                    alert('O campo nome deve estar preenchido\nO preço da fruta não pode ser menor que 0 e precisa ser válido')
                 }
             },
 
