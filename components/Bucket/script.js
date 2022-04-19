88b1087bc11b702bbea282e6dc0dfae0d75627fd
edAndrade <edmilisonwork@gmail.com>
2022-04-19 10:36:27 +0100

    getting the bucket total

--- components/Bucket/script.js
+++ components/Bucket/script.js
@@ -13,6 +13,18 @@ export default {
 
         removeBucket(){
             this.$store.commit('buckets/REMOVE_BUCKET', {bucket_id: this.bucket.id})
+        },
+
+        getTotal(){
+            if(this.bucket.fruits.length > 0){
+
+                return this.bucket.fruits.reduce( (fruit, sum) => {
+                    return sum+fruit.fruit_price
+                })
+                
+            }else{
+                return 0
+            }
         }
     }
 }
