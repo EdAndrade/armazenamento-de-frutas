d3c0383a4f3b2b51ec94f6bac4b65e3c50977d62
edAndrade <edmilisonwork@gmail.com>
2022-04-20 01:06:39 +0100

    implemented state persistence

--- store/index.js
+++ store/index.js
@@ -1,3 +1,28 @@
-export const state = () => ({
-    
+import CreatePersistedState from 'vuex-persistedstate'
+import secureLS from 'secure-ls'
+
+const ls = new secureLS({ isCompression: true })
+
+export const state = () => ({})
+
+const indexState = CreatePersistedState({
+
+    key: '!2)@#213fd',
+
+    paths: [
+        'buckets.buckets',
+        'buckets.bucket_id_track',
+        'fruits.fruit_id_track',
+        'fruits.fruits'
+    ],
+
+    storage: {
+        getItem: (key) => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: (key) => ls.remove(key)
+    }
 })
+
+export const plugins = [
+    indexState
+]
